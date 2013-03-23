c7ac8d6bc1451e90b1e540de28b65ce944a7d94c
cainrus <cainrus@gmail.com>
2013-03-23 18:28:42 +0400

    fix redis connection for heroku

--- classes/redisConnection.js
+++ classes/redisConnection.js
@@ -1,18 +1,10 @@
 // db.js
 var redis = require('redis');
-var url = require('url');
-var redisURL = url.parse(process.env.REDISCLOUD_URL);
+var redisURL = require('url').parse(process.env.REDISCLOUD_URL);
 var client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
-client.auth(redisURL.auth.split(":")[1]);
-
-/*
-var environmentFile = '/home/dotcloud/environment.json';
-var environment = JSON.parse(require('fs').readFileSync(environmentFile));
-var redishost = environment['DOTCLOUD_DATA_REDIS_HOST'];
-var redisport = environment['DOTCLOUD_DATA_REDIS_PORT'];
-var redispass = environment['DOTCLOUD_DATA_REDIS_PASSWORD'];
-var client = redis.createClient(redisport, redishost);
-client.auth(redispass);
-*/
+client.auth(redisURL.auth.split(":")[1], function(err){
+	if (err) { throw err; }
+	else { console.log('Redis db auth state: success.'); }
+});
 
 module.exports = client;
