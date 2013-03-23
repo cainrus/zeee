f1b5bbbca68fd73f9601e32f0072dde6c5f1f46c
cainrus <cainrus@gmail.com>
2013-03-23 14:32:03 +0400

    more logs

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
+console.log('starging app.');
 var
     express = require('express'),
     connect = require('connect'),
-    db = require("./classes/redisConnection.js"),
     counter = require('./classes/counter.js'),
     common = require('./public/js/common.js'),
     conf = require('./settings/environment.js');
@@ -10,21 +10,11 @@ var
 
 //global.everyauth = require('everyauth');
 
+console.log('starging app db.');
+var db = require("./classes/redisConnection.js");
 db.on("error", function (err) {
   console.log("redis error: " + err);
   process.exit(1);
-});
-
-// init redis dependent stuff
-var dbDependedInit = require('underscore').once(function() {
-    global.urlCounter = new counter({name: 'url', client: db});
-    var site_vhosts = [];
-
-    site_vhosts.push(express.vhost('cdn.' + conf.domain, require('./apps/assetProvider/app.js')));
-    site_vhosts.push(express.vhost('*.' + conf.domain, require('./apps/short/app.js')));
-
-    vhost = express.createServer.apply(this, site_vhosts);
-    vhost .listen(conf.port);
 });
 
 db.on('connect', function() {
@@ -32,10 +22,19 @@ db.on('connect', function() {
     dbDependedInit();
 });
 
-db.on('error', function() {
-    console.log('Redis db error: ' + arguments.slice(0).join(', '));
+// init redis dependent stuff
+var dbDependedInit = require('underscore').once(function() {
+    console.log('initing urlCounter');
+    global.urlCounter = new counter({name: 'url', client: db});
+	
+	console.log('initing vhosts');
+    var site_vhosts = [];
+    site_vhosts.push(express.vhost('cdn.' + conf.domain, require('./apps/assetProvider/app.js')));
+    site_vhosts.push(express.vhost('*.' + conf.domain, require('./apps/short/app.js')));
+    vhost = express.createServer.apply(this, site_vhosts);
+    vhost .listen(conf.port);
+	console.log('express is listening port: '+conf.port);
 });
-
 
 
 
