f8dca1e1cc3b20f2f004745f38aa9d9adcfd06bb
cainrus <cainrus@gmail.com>
2013-08-25 00:48:01 +0400

    fix

--- settings/environment.js
+++ settings/environment.js
@@ -7,7 +7,7 @@
     conf.env = process.env.NODE_ENV = process.env.NODE_ENV || 'production';
     conf.domain = process.env.NODE_ENV === 'production' ? 'ze.ee' : 'localhost.loc';
     conf.port = process.env.PORT || 5000;
-    conf.domainWithPort = conf.domain + (conf.port && conf.port !== 80 ? (':' + conf.port) : '');
+    conf.domainWithPort = conf.domain + (conf.env === 'production' ? '' : (conf.port && conf.port !== 80 ? (':' + conf.port) : ''));
     conf.subdomain = 'www';
     conf.outerPort = 80;
     conf.protocol = 'http://';
