76b2993e5a22314c11c17836a44d3638e1eb0a95
cainrus <cainrus@gmail.com>
2013-08-25 00:20:50 +0400

    'fix'

--- db/redisConnection.js
+++ db/redisConnection.js
@@ -1,14 +1,21 @@
 // db.js
-var client, redis = require('redis');
-if (process.env.NODE_ENV !== 'production') {
-	client = redis.createClient();
-} else {
-	var redisURL = require('url').parse(process.env.REDISCLOUD_URL);
-	client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
-	client.auth(redisURL.auth.split(":")[1], function(err){
-		if (err) { throw err; }
-		else { console.log('Redis db auth state: success.'); }
-	});
-}
+(function () {
+    'use strict';
+    var client, redis = require('redis');
+    if (process.env.NODE_ENV === 'production') {
+        var redisURL = require('url').parse(process.env.REDISCLOUD_URL);
+        client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+        client.auth(redisURL.auth.split(":")[1], function (err) {
+            if (err) {
+                throw err;
+            }
+            else {
+                console.log('Redis db auth state: success.');
+            }
+        });
+    } else {
+        client = redis.createClient();
+    }
 
-module.exports = client;
+    module.exports = client;
+})();
