a546cf09070dd60ea24119b95c019ef3bc2d1d7d
cainrus <cainrus@gmail.com>
2013-08-25 00:25:15 +0400

    fix

--- db/redisConnection.js
+++ db/redisConnection.js
@@ -3,6 +3,7 @@
     'use strict';
     var client, redis = require('redis');
     if (process.env.NODE_ENV === 'production') {
+        console.log('Redis connect url: ' + process.env.REDISCLOUD_URL);
         var redisURL = require('url').parse(process.env.REDISCLOUD_URL);
         client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
         client.auth(redisURL.auth.split(":")[1], function (err) {
