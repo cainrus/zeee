196b51735cc68197a0c4124ec899f3cf26839146
Sergei Tarassov <starassov@leongaming.com>
2016-01-09 17:10:17 +0200

    fix domain resolve

--- server.js
+++ server.js
@@ -8,10 +8,12 @@ require("./db/redis.init.js")(conf);
 
 // Initialize vhosts.
 global.server = express.createServer();
+var assetProvider = require('./apps/assetProvider/app.js')(conf);
+var shortenerApp = require('./apps/short/app.js')(conf);
 global.server
-    .use(express.vhost('cdn.' + conf.domain, require('./apps/assetProvider/app.js')(conf)))
-    .use(express.vhost('*.' + conf.domain, require('./apps/short/app.js')(conf)));
-
+    .use(express.vhost('cdn.' + conf.domain, assetProvider))
+    .use(express.vhost('*.' + conf.domain, shortenerApp))
+    .use(express.vhost(conf.domain, shortenerApp));
 
 console.log('listen: cdn.' + conf.domain + ':' + conf.port);
 console.log('listen: *.' + conf.domain + ':' + conf.port);
